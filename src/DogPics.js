c36f94c8d26a57299452ec8aa7f7179cd3fa0746
rasmi <rasminoel@gmail.com>
2023-10-28 15:09:16 +0300

    Done with Assignment

--- src/DogPics.js
+++ src/DogPics.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 // WARNING: this useEffect will run in an infinite loop!
 // to fix, pass an empty array as the second argument for useEffect
+
 function DogPics() {
   const [images, setImages] = useState([]);
 
